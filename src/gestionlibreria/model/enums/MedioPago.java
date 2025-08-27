eec3e535ca69dc38b86b1687cf828dd26d16ae4f
Your Name <you@example.com>
2025-08-26 23:25:16 -0400

    Lista la estructura del proyecto.

---
+++ src/gestionlibreria/model/enums/MedioPago.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package gestionlibreria.model.enums;
+
+/**
+ *
+ * @author Juaaan
+ */
+public enum MedioPago {
+    
+}
